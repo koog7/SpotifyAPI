cb4b6993f004c2786000128e81faf63a04e1bbf7
koog7 <k.ognev709@mail.com>
2024-08-31 14:40:29 +0600

    Добавлен метод get с возможность передать параметр

--- routes/AlbumsRoutes.ts
+++ routes/AlbumsRoutes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import {imagesUpload} from "../multer";
 import Album from "../models/Albums";
+import Artist from "../models/Artists";
+import ArtistsRouter from "./ArtistsRoutes";
 
 
 const AlbumsRouter = express.Router();
@@ -22,6 +24,40 @@ AlbumsRouter.post( '/albums', imagesUpload.single('photo'), async (req, res )=>{
     }
 });
 
+
+AlbumsRouter.get( '/albums', async (req, res )=>{
+    const {artist} = req.query;
+
+    if(artist){
+        try {
+            const albums = await Album.find({artistId: artist});
+
+            const albumInfo = albums.map(album => ({
+                _id: album._id,
+                title: album.title,
+                dataRelease: album.dataRelease,
+                photo: album.photo,
+            }));
+
+            res.send(albumInfo)
+        }catch (e) {
+            res.send('cant find artist')
+        }
+    }else {
+        const albums = await Album.find();
+
+        const albumInfo = albums.map(album => ({
+            _id: album._id,
+            title: album.title,
+            dataRelease: album.dataRelease,
+            photo: album.photo,
+        }));
+
+        res.send(albumInfo)
+    }
+
+
+});
 
 export default AlbumsRouter;
 
