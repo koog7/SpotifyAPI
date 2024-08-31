69354ebd5bc301e8059cff6eb275c22078d206c7
koog7 <k.ognev709@mail.com>
2024-08-31 11:03:41 +0600

    Инициализирован бэк , установлены зависимости

---
+++ routes/SpotifyRoutes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const SpotifyRouter = express.Router();
+SpotifyRouter.use(express.json());
+
+SpotifyRouter.post( '/', (res, req )=>{
+
+});
+
+
+export default SpotifyRouter;
