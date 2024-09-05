346242b43f8465ad846036ff2580fababfb16f51
koog7 <k.ognev709@mail.com>
2024-09-05 13:59:46 +0600

    Добавлен метод post для создания прослушаных треков

--- routes/tracksRouter.ts
+++ routes/tracksRouter.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import Track from "../models/Tracks";
+import User from "../models/Users";
+import TrackHistory from "../models/TrackHistory";
+import {randomUUID} from "crypto";
 
 const tracksRouter = express.Router();
 tracksRouter.use(express.json());
@@ -51,6 +54,35 @@ tracksRouter.get( '/tracks', async (req, res )=>{
     }
 
 
+});
+
+
+
+tracksRouter.post( '/track_history', async (req, res )=>{
+    const token = req.get('Authorization');
+    const trackId = req.body.trackId;
+    if(!token){
+        return res.status(401).send({error: 'Unauthorized'});
+    }
+    try {
+        const user = await User.findOne({token:token});
+
+        if(!user){
+            return res.status(400).send({error:'User not found'})
+        }
+
+        const userHistory = new TrackHistory({
+            userId: user._id,
+            trackId: trackId,
+            datetime: new Date().toISOString(),
+        });
+
+        await userHistory.save()
+
+        res.send(userHistory)
+    }catch (e) {
+        res.send('cant be added in history')
+    }
 });
 
 export default tracksRouter;
